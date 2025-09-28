4e0d4087722ac7924bd3ca56d253321f0795ec6d
ml-1113 <mlee0162@student.monash.edu>
2025-09-28 19:39:39 +0800

    Updated generate_html.py file - Added loop to generate HTML for each post displaying the post title and the image

--- generate_html.py
+++ generate_html.py
@@ -3,6 +3,12 @@ import json
 with open("output.json", "r", encoding = "utf-8") as f:
     posts = json.load(f)
 
-with open("index.html", "w", encoding="utf-8") as f:
+with open("image.html", "w", encoding="utf-8") as f:
     f.write("<!DOCTYPE html>\n<html>\n<head><title>Reddit Images</title></head>\n<body>\n")
     f.write("<h1>Reddit Image Posts</h1>\n")
+
+    for post in posts:
+        f.write(f"<h2>{post['post_title']}</h2>\n")
+        f.write(f"<img src = '{post['image_url']}' style = 'max-width:400px;'><br>\n")
+    
+    f.write("</body>\n</html>")
